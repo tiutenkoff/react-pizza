0f6c86bd76ba4b9e7c1433774e349f72fa762fbd
AlekseiTiutenkov@mail.ru <AlekseiTiutenkov@mail.ru>
2021-07-16 20:52:01 +0700

    Added a few components, made visual switching of categories

---
+++ src/components/Categories.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function Categories({ items, onClickItem }) {
+    const [activeItem, setActiveItem] = useState(null)
+
+    const onSelectItem = index => {
+        setActiveItem(index)
+    }
+
+    console.log(items)
+    return (
+        <div className="categories">
+            <ul>
+                <li
+                    onClick={() => onSelectItem(null)}
+                    className={activeItem === null ? 'active' : ''}
+                >Все</li>
+                {items &&
+                    items.map((name, index) => (
+                        <li
+                            className={activeItem === index ? 'active' : ''}
+                            onClick={() => onSelectItem(index)}
+                            key={`${name}_${index}`}
+                        >{name}</li>
+                    ))
+            }
+            </ul>
+        </div>
+    )
+}
+
+export default Categories;
